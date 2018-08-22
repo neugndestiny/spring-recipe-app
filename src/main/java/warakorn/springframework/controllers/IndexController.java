b3efc6d450923b73a2bad4e11e9284d3da51c7d9
neugndestiny <neungdestiny12@gmail.com>
2018-08-22 17:43:16 +0700

    Create notes and recipes simple class initialize variable and then create getter and setter functions

--- src/main/java/warakorn/springframework/controllers/IndexController.java
+++ src/main/java/warakorn/springframework/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"","/","index"})
     public String getIndexPage() {
+        System.out.println("Send massage to say... 5555555555");
         return "index";
     }
 }
