6e29f850a1ed070b968f95b67131ee09f57d16f7
neugndestiny <neungdestiny12@gmail.com>
2018-08-24 10:57:16 +0700

    Refractor classes with lombok

--- src/main/java/warakorn/springframework/domain/Category.java
+++ src/main/java/warakorn/springframework/domain/Category.java
@@ -1,7 +1,6 @@
 package warakorn.springframework.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -19,27 +18,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
